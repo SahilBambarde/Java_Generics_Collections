a1e20cf04681c4302e88c587642866ec4712094d
sahil_bambarde <sahil_bambarde>
2024-04-09 11:11:17 +0530

    Second commit

--- src/main/java/org/example/DataStructureDemo.java
+++ src/main/java/org/example/DataStructureDemo.java
@@ -1,2 +1,38 @@
-package org.example;public class DataStructureDemo {
+package org.example;
+
+import java.util.ArrayList;
+
+public class DataStructureDemo {
+    public static void main(String[] args){
+        String[] strArray = new String[20];
+        Employee[] empArray = new Employee[20];
+        strArray[0]="Sahil";
+
+        //strArray[1]=new Employee();
+        //org.example.Employee cannot be converted to java.lang.String
+
+        ArrayList l = new ArrayList();
+        //int --> Integer
+        l.add("Sahil");
+        l.add(new Employee());
+        //ArrayList -> it can accept both types og Objects
+
+        //String name = l.get(0);
+        //java: incompatible types: java.lang.Object cannot be converted to java.lang.String
+
+        //String name = (String) l.get(0);
+        //this will work
+
+        //Employee name = (Employee) l.get(0);
+        //class java.lang.String cannot be cast to class org.example.Employee
+        // (java.lang.String is in module java.base of loader 'bootstrap'; org.example.Employee
+        // is in unnamed module of loader 'app')
+        //	at org.example.DataStructureDemo.main(DataStructureDemo.java:26)
+
+
+
+
+
+
+    }
 }
